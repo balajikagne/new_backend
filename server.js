84cc377c696215493d181529c5f554cabe77c44d
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-12-13 17:09:21 +0530

    Update server.js port is seted

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app=express()
 const cors=require('cors')
-
+const port=process.env.PORT || PORT;
 require('dotenv').config()
 const mongoose=require('mongoose')
 const DB=process.env.MODGODB
@@ -62,7 +62,7 @@ app.get('/',(req,res)=>{
 //    }
 // })
 
-const port=process.env.PORT || PORT;
+
 
 app.listen(port,()=>{
     console.log("connected successfully :",port)
