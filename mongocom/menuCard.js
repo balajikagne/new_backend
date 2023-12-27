6a1e86285e2c2d24665d6b419e35687f50fb895c
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-12-28 04:33:11 +0530

    Update menuCard.js degv

--- mongocom/menuCard.js
+++ mongocom/menuCard.js
@@ -1,17 +1,20 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const UoodSchema=new mongoose.Schema({
-    name:{type:String,require},
-    _id:{type:String},
-    varients:[],
-    prices:[],
-    img:{type:String,require},
-    dsc:{type:String,require},
-    rate:{type:Number,require},
-    country:{type:String}
-},{
-    timestamps:true,
-})
+const UoodSchema = new mongoose.Schema({
+    name: { type: String, required: true }, // Fixed the 'require' typo
+    _id: { type: mongoose.Schema.Types.ObjectId,auto: true },
+    varients: [],
+    prices: [],
+    img: { type: String, required: true },
+    dsc: { type: String, required: true },
+    rate: { type: Number, required: true },
+    country: { type: String },
+    category: { type: String, required: true },
+    location: { type: String },
+    stock: { type: Boolean }
+}, {
+    timestamps: true,
+});
 
-let UenCard=new mongoose.model("menus",UoodSchema)
-module.exports= UenCard;
+let UenCard = new mongoose.model("menus", UoodSchema);
+module.exports = UenCard;
