429b4963b3b6e808235f5938305d7bbdc1af1a22
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-02-05 05:36:07 +0530

    DDSFGFGBV EREFDFVDSF

---
+++ mongocom/beautyModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UoodSchema = new mongoose.Schema({
+    name: { type: String, required: true }, // Fixed the 'require' typo
+    _id: { type: mongoose.Schema.Types.ObjectId,auto: true },
+    prices: [],
+    services:[],
+    servicesImages:[],
+    img: { type: String, required: true },
+    dsc: { type: String, required: true },
+    rate: { type: Number, required: true },
+    country: { type: String },
+    category: { type: String, required: true },
+    location: { type: String },
+    stock: { type: Boolean }
+}, {
+    timestamps: true,
+});
+
+let UenCard = new mongoose.model("beauties", UoodSchema);
+module.exports = UenCard;
