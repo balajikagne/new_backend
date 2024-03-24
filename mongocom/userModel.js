98811baf77ba574f90f23e14a5f65235506c0965
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-03-24 17:22:36 +0530

    Update userModel.js cnjdjfd

--- mongocom/userModel.js
+++ mongocom/userModel.js
@@ -2,6 +2,8 @@ const mongoose=require("mongoose")
 
 const userSchema=new mongoose.Schema({
     name:{type:String,require},
+    bdate:{type:String,require},
+    location:{type:String,require},
     mobNumber:{type:String,require},
     password:{type:String,require},
     isAdmin:{type:Boolean,require ,default:false},
